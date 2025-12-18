569a6dacadcc19add2b0c3a4cf622f795b1f930e
Omar Faruk Rakin <farukrakin09@gmail.com>
2025-12-18 16:12:22 +0600

    edited code

--- grouptask.cpp
+++ grouptask.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 struct Edge
 {
     int u, v;
@@ -24,14 +23,12 @@ int func(vector<Edge> edges, int n)
             if (e.v == v)
                 e.v = u;
         }
-
         vector<Edge> newEdges;
         for (auto &e : edges)
         {
             if (e.u != e.v)
                 newEdges.push_back(e);
         }
-
         edges = newEdges;
         vertices--;
     }
@@ -42,28 +39,21 @@ int main()
 {
     ifstream fin("input.txt");
     ofstream fout("output.txt");
-
     int n, m;
     fin >> n >> m;
-
     vector<Edge> edges(m);
     for (int i = 0; i < m; i++)
     {
         fin >> edges[i].u >> edges[i].v;
     }
-
     int trials = n * n;
     int minCut = INT_MAX;
-
     for (int i = 0; i < trials; i++)
     {
         minCut = min(minCut, func(edges, n));
     }
-
     fout << "Minimum Cut  " << minCut << endl;
-
     fin.close();
     fout.close();
-
     return 0;
 }
